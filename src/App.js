f16890b1099bf40c37074a7576443778ccfd06b5
Barnik Maity <maitybarnik@gmail.com>
2022-05-24 00:56:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import TextForm from './components/TextForm'
 import About from './components/About'
 import React,{useState} from 'react'
 import Alert from './components/Alert'
+import { BrowserRouter as Router,
+  Routes, 
+  Route, 
+  Link } from "react-router-dom";
+
+
 
  function App() {
    const [mode, setMode] = useState('light')
@@ -25,21 +31,28 @@ import Alert from './components/Alert'
        setMode('dark')
        document.body.style.backgroundColor='grey'
        showAlert("Dark Mode has been enabled","success")
+       document.title="TextUtils-Dark Mode"
      }
      else {
        setMode('light')
        document.body.style.backgroundColor='white'
        showAlert("Light Mode has been enabled","success")
+       document.title="TextUtils-Light Mode"
      }
    }
   return (
     <>
+   <Router>
     <Navbar title="TextUtils2" about="About Barnik" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-<TextForm heading="Enter the text to analyze below" mode={mode}/>
+    <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode}/>}/>
+        </Routes>
 {/*<About/>*/}
 </div>
+</Router>
     </>
   );
 }
